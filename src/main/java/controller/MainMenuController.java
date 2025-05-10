d59606835e17b8a42276f5469676bf6e45b1f8b4
xaxsd <xaxsdxaxsd76@gmail.com>
2025-05-10 08:11:07 +0100

    1

--- src/main/java/controller/MainMenuController.java
+++ src/main/java/controller/MainMenuController.java
@@ -1,4 +1,5 @@
 package main.java.controller;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -9,11 +10,18 @@ public class MainMenuController {
     @FXML private VBox menuContainer;
     @FXML private Label playerInfo;
     
-    private Player currentPlayer;
+    private Player currentPlayer;  // Keep this if you'll need player data later
     
     public void setPlayerData(Player player) {
         this.currentPlayer = player;
         playerInfo.setText("Гравець: " + player.getUsername());
+        // You can add more player-related UI updates here
+    }
+    
+    @FXML
+    private void startNewGame() {
+        // Example usage of currentPlayer
+        System.out.println("Starting new game for: " + currentPlayer.getUsername());
     }
     
     @FXML
